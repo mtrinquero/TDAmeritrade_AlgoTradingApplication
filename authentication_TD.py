523a9f931ec8cd936de9dd99205a1a696bba9e54
Mark Trinquero <mtrinquero@Marks-Air.attlocal.net>
2018-04-02 13:40:22 -0400

    adding JSON for various API responses and requests

--- authentication_TD.py
+++ authentication_TD.py
@@ -1,3 +1,29 @@
+## Mark Trinquero
+## TD Ameritrade Python Trading Bot
+## ----------------------------------------
+## Authentication Code
+## Reference: https://developer.tdameritrade.com/content/authentication-sample-python-3
+
+## In Browser Testing
+## https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=Redirect URI&client_id=OAuth User ID
+
+## ----------------------------------------
+## Setup Notes
+
+## Generating a self-signed certificate
+## The openssl command below will generate key and certificate files you will need later. Put them in a location accessible to the Node app.
+
+## openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
+## Note: this is specific to apps running on the local machine, a response is sent to the browser only to show it's working.
+
+## Setting up the Python environment
+## Create a directory for this app and run the commands below
+## python -m pip install requests
+
+## ----------------------------------------
+
+
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 import requests
